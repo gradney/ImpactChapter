3a586e5fdcfc51c34c95a9f796db4e331bb52852
G Radney <gradney@gmail.com>
2016-02-05 16:36:44 -0500

    Adding Video Gallery page

---
+++ application/views/page/videos.php
@@ -0,0 +1,188 @@
+<?php
+/******************************************************************************
+ * Purpose: To provide videos which can speak louder than a picture.
+ *===================================================================
+ * Types of Videos:
+ *------------------
+ * 1) Biblical Videos (*-gospel)
+ * 2) "How To"/Educational Videos (*-howto)
+ * 3) Encouragement/Motivational Videos (*-motive)
+ * - Draw Your Future - https://youtu.be/zESeeaFDVSw
+ * 4) Promotional Videos (*-promo)
+ *****************************************************************************/
+?>
+
+<?php/** PAGE TOP **********************************************************/?>
+
+<?php/** PAGE BODY *********************************************************/?>
+<div class="container">
+  <div class="row">
+      <ul id="video-categories" class="nav nav-tabs nav-justified">
+        <li class="col-md-3 active">
+          <div class="text-size-40 text-nowrap">
+            <a class="nounderline" data-toggle="tab" href="#video-panel-promo"><i class="fa fa-video-camera"></i>&nbsp;Promos</a>
+          </div>
+        </li>
+        <li class="col-md-3">
+          <div class="text-size-40 text-nowrap">
+            <a class="nounderline" data-toggle="tab" href="#video-panel-gospel"><i class="fa fa-book"></i>&nbsp;Good News</a>
+          </div>
+        </li>
+        <li class="col-md-3">
+          <div class="text-size-40 text-nowrap">
+            <a class="nounderline" data-toggle="tab" href="#video-panel-grow"><i class="fa fa-video-camera"></i>&nbsp;Let's Grow</a>
+          </div>
+        </li>
+        <li class="col-md-3">
+          <div class="text-size-40 text-nowrap">
+            <a class="nounderline" data-toggle="tab" href="#video-panel-motive"><i class="fa fa-video-camera"></i>&nbsp;Encouragement</a>
+          </div>
+        </li>
+      </ul>
+  </div><?php/*end:row*/?>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="tab-content">
+
+        <div id="video-panel-promo" class="tab-pane fade in active">
+          <!--Animate this-->
+          <h3 class="video-gallery-heading">3, 2, 1... Showtime!</h3>
+        
+<?php /**********************************************************
+          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Carousel Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            </ol>
+            <!-- Carousel Slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                   <!--div align="center">
+                     <video width="320" height="240" controls>
+                       <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" width="1024" height="552" type="video/mp4"><!-poster="path/to/img/of/video"/->
+                       Your browser does not support the video tag.
+                    </video>
+                    </div-->
+                    <div align="center">
+                      <iframe width="560" height="315" src="https://www.youtube.com/embed/lJOSHojiv3k" frameborder="0" allowfullscreen>Your browser does not support the iframe tag. Please upgrade your browser.</iframe>
+                    </div>
+                    <div class="carousel-caption">
+                      <h3>hello</h3>
+                      <p>hello</p>
+                    </div>
+              </div> 
+              <div class="item">
+                    <img src="http://dummyimage.com/1024x552/000/fff" class="img-responsive" alt="asfds">
+                    <div class="carousel-caption">
+                      <h3>hello</h3>
+                      <p>hello.</p>
+                    </div>
+              </div>
+              <!-- Carousel Controls -->
+              <a class="left carousel-control" href="#carouselHelp" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#carouselHelp" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div><!--end:carousel-inner-->
+          </div><!--end:carousel-example-->
+***************************************************************/
+?>
+          <!--/** Video Gallery
+               * - original YouTube iframe width:560, height:315;
+               * - nice look YouTube iframe width:360, height:190;
+          -->
+          <div class="video-gallery">
+            <div class="row">
+              <div class="col-md-4">
+                <div id="impactconference13" class="well">
+                  <!-- 4:3 aspect ratio -->
+                  <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/BFjxAhcj1GA" allowfullscreen></iframe>Your browser does not support the iframe tag. Please upgrade your browser.</iframe>
+                  </div>
+                  <h3 class="text-center">Impact '13 "Break Free"</h3>
+                  <h3 class="text-center"><span class="label label-primary">National Conference</span></h3>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div id="impactconference12" class="well">
+                  <!-- 4:3 aspect ratio -->
+                  <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/lJOSHojiv3k" allowfullscreen>Your browser does not support the iframe tag. Please upgrade your browser.</iframe>
+                  </div>
+                  <h3 class="text-center">Impact '12 "M.I.A."</h3>
+                  <h3 class="text-center"><span class="label label-primary">National Conference</span></h3>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div id="impactconference11" class="well">
+                  <!-- 4:3 aspect ratio -->
+                  <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/oMai1dUbUcM" allowfullscreen>Your browser does not support the iframe tag. Please upgrade your browser.</iframe>
+                  </div>
+                  <h3 class="text-center">Impact '11 "The Remedy"</h3>
+                  <h3 class="text-center"><span class="label label-primary">National Conference</span></h3>
+                </div>
+              </div>
+            </div><?php /*<!--end:row-->*/ ?>
+            <div class="row">
+              <div class="col-md-4">
+                <div id="impactconference10" class="well">
+                  <!-- 4:3 aspect ratio -->
+                  <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/6YJCDsjPyJ0" allowfullscreen></iframe>Your browser does not support the iframe tag. Please upgrade your browser.</iframe>
+                  </div>
+                  <h3 class="text-center text-nowrap">Impact '10 "The Come Back"</h3>
+                  <h3 class="text-center"><span class="label label-primary">National Conference</span></h3>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div id="impactconference08" class="well">
+                  <!-- 4:3 aspect ratio -->
+                  <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/6O5hLie9ZW4" allowfullscreen>Your browser does not support the iframe tag. Please upgrade your browser.</iframe>
+                  </div>
+                  <h3 class="text-center text-nowrap">Impact '08 "Mission Possible"</h3>
+                  <h3 class="text-center"><span class="label label-primary">National Conference</span></h3>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div id="impactconference11" class="well">
+                  <!-- 4:3 aspect ratio -->
+                  <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/oMai1dUbUcM" allowfullscreen>Your browser does not support the iframe tag. Please upgrade your browser.</iframe>
+                  </div>
+                  <h3 class="text-center">Impact '11 "The Remedy"</h3>
+                  <h3 class="text-center"><span class="label label-primary">National Conference</span></h3>
+                </div>
+              </div>
+            </div><?php /*<!--end:row-->*/ ?>
+          </div><?php /*<!--end:video-gallery-->*/ ?>
+
+        </div><!--end:video-panel-promo-->
+
+        <div id="video-panel-gospel" class="tab-pane fade">
+          <h3>Word!</h3>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div id="video-panel-grow" class="tab-pane fade">
+          <h3>Getting Greener</h3>
+          <small>We can do this and so can you, homie!</small>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+        </div>
+        <div id="video-panel-motive" class="tab-pane fade">
+          <h3>Be Encouraged, Fam</h3>
+          <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        </div>
+      </div><?php/*end:tab-content*/?>
+
+    </div><?php/*end:col*/?>
+  </div><?php/*end:row*/?>
+</div><?php/*end:container*/?>
+
+<?php/** PAGE BOTTOM *******************************************************/?>
+
