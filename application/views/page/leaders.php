edbfecc67f9b2656e334bc6f487ef8591d0bc390
G-Rad <gradney@gmail.com>
2016-10-17 22:15:15 -0400

    Leaders page under development

--- application/views/page/leaders.php
+++ application/views/page/leaders.php
@@ -2,23 +2,84 @@
   <!--link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css"-->
 
   <div id="leaders-headline" class="container">
-    <div class="row top-buffer-20">
+    <div class="row">
       <div class="col-xs-12">
-        <h2>Student Leaders</h2>
+        <h1 style="margin-top: 0px;text-align: center;">Student Leaders</h1>
+        <p style="font-size: 1.25em;"><em>"Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek."</em> - President Barack Obama</p>
       </div>
       <!--hr style="border-color: black; height:10px"/-->
     </div>
   </div>
 
   <div id="leaders-profiles" class="container">
-    <div class="row">
-      <div class="col-xs-3">student 1 profile
+    <div class="row top-buffer-20">
+      <div class="col-md-3" style="margin-bottom: 20px;">
+          <div class="card" style="text-align: center;">
+            <img class="card-img-top" src="img/pic/profiles/grant_headshot.png" alt="Grant Edwards">
+            <div class="card-block">
+              <h4 class="card-title">Grant Edwards</h4>
+              <p class="card-text">
+                <ul style="list-style: none; padding-left: 0px;">
+                  <li>President/COO</li>
+                  <li>c/o 2019</li>
+                  <li>Supply Chain Mgmt</li>
+                  <li>FL</li>
+                </ul>
+              </p>
+              <a href="#" class="btn btn-primary">Contact</a>
+            </div>
+          </div>
       </div>
-      <div class="col-xs-3">student 2 profile
+      <div class="col-md-3" style="margin-bottom: 20px;">
+          <div class="card" style="text-align: center;">
+            <img class="card-img-top" src="img/pic/profiles/kyle_headshot.png" alt="Kyle McNaughton">
+            <div class="card-block">
+              <h4 class="card-title">Kyle McNaughton</h4>
+              <p class="card-text">
+                <ul style="list-style: none; padding-left: 0px;">
+                  <li>Vice President</li>
+                  <li>c/o 2019</li>
+                  <li>Finance</li>
+                  <li>New Jersey</li>
+                </ul>
+              </p>
+              <a href="#" class="btn btn-primary">Contact</a>
+            </div>
+          </div>
       </div>
-      <div class="col-xs-3">student 3 profile
+      <div class="col-md-3" style="margin-bottom: 20px;">
+          <div class="card" style="text-align: center;">
+            <img class="card-img-top" src="img/pic/profiles/malachia_headshot.png" alt="Malachia Foard">
+            <div class="card-block">
+              <h4 class="card-title">Malachia Foard</h4>
+              <p class="card-text">
+                <ul style="list-style: none; padding-left: 0px;">
+                  <li>Treasurer/CMO</li>
+                  <li>c/o 2019</li>
+                  <li>Finance</li>
+                  <li>Arizona</li>
+                </ul>
+              </p>
+              <a href="#" class="btn btn-primary">Contact</a>
+            </div>
+          </div>
       </div>
-      <div class="col-xs-3">student 4 profile
+      <div class="col-md-3" style="margin-bottom: 20px;">
+          <div class="card" style="text-align: center;">
+            <img class="card-img-top" src="img/pic/profiles/tracy_headshot.png" alt="Tracy Okoroh">
+            <div class="card-block">
+              <h4 class="card-title">Tracy Okoroh</h4>
+              <p class="card-text">
+                <ul style="list-style: none; padding-left: 0px;">
+                  <li>Secretary</li>
+                  <li>c/o 2019</li>
+                  <li>Info. Sys.</li>
+                  <li>Maryland</li>
+                </ul>
+              </p>
+              <a href="#" class="btn btn-primary">Contact</a>
+            </div>
+          </div>
       </div>
     </div>
   </div>
